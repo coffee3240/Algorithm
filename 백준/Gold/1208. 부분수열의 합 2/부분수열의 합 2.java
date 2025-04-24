3129f4e7b0865aeb9246f39256f8a2ddfb881570
coffee3240 <seungbok3240@gmail.com>
2025-04-25 02:24:28 +0900

    [Gold I] Title: 부분수열의 합 2, Time: 132 ms, Memory: 29864 KB -BaekjoonHub

--- 백준/Gold/1208. 부분수열의 합 2/부분수열의 합 2.java
+++ 백준/Gold/1208. 부분수열의 합 2/부분수열의 합 2.java
@@ -4,19 +4,21 @@ import java.io.InputStreamReader;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.StringTokenizer;
-import java.util.function.Consumer;
 
 public class Main {
+    private static final int OFFSET = 2_000_000;
 
+    static int n, s;
     static int[] arr;
+    static int[] counts = new int[OFFSET * 2 + 1];
     static long answer = 0;
 
     public static void main(String[] args) throws IOException {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(bf.readLine());
 
-        int n = Integer.parseInt(st.nextToken());   // 정수의 개수
-        int s = Integer.parseInt(st.nextToken());   // 타겟
+        n = Integer.parseInt(st.nextToken());   // 정수의 개수
+        s = Integer.parseInt(st.nextToken());   // 타겟
 
         arr = new int[n];
         st = new StringTokenizer(bf.readLine());
@@ -26,19 +28,31 @@ public class Main {
 
         Map<Integer, Integer> countMap = new HashMap<>();
 
-        comb(0, n / 2, 0, sum -> countMap.put(sum, countMap.getOrDefault(sum, 0) + 1));
-        comb(n / 2, n, 0, sum -> answer += countMap.getOrDefault(s - sum, 0));
+        combHalf(0,0);
+        combHalfAndMatch(n / 2,0);
 
         System.out.println(s == 0 ? answer - 1 : answer);
     }
 
-    private static void comb(int startIdx, int endIdx, int sum, Consumer<Integer> callback) {
-        if (startIdx == endIdx) {
-            callback.accept(sum);
+    private static void combHalfAndMatch(int idx, int sum) {
+        if (idx == n) {
+            int other = s - sum + OFFSET;
+            if (0 <= other && other <= OFFSET * 2) {
+                answer += counts[other];
+            }
+            return;
+        }
+        combHalfAndMatch(idx + 1, sum);
+        combHalfAndMatch(idx + 1, arr[idx] + sum);
+    }
+
+    private static void combHalf(int idx, int sum) {
+        if (idx == n / 2) {
+            counts[sum + OFFSET]++;
             return;
         }
 
-        comb(startIdx + 1, endIdx, sum, callback);
-        comb(startIdx + 1, endIdx, sum + arr[startIdx], callback);
+        combHalf(idx + 1, sum);
+        combHalf(idx + 1, sum + arr[idx]);
     }
 }
