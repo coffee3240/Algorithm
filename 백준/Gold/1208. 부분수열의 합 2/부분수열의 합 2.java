bc825f7a542a911c8efaceb9351e1cdc07c71214
coffee3240 <seungbok3240@gmail.com>
2025-04-25 01:43:57 +0900

    [Gold I] Title: 부분수열의 합 2, Time: 648 ms, Memory: 94260 KB -BaekjoonHub

--- 백준/Gold/1208. 부분수열의 합 2/부분수열의 합 2.java
+++ 백준/Gold/1208. 부분수열의 합 2/부분수열의 합 2.java
@@ -1,16 +1,16 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.StringTokenizer;
+import java.util.function.Consumer;
 
-/**
- * 1 <= n <= 40
- *
- * 조합 => 2^40
- * mitm => 2^20 * 2
- */
 public class Main {
+
+    static int[] arr;
+    static long answer = 0;
+
     public static void main(String[] args) throws IOException {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(bf.readLine());
@@ -18,81 +18,27 @@ public class Main {
         int n = Integer.parseInt(st.nextToken());   // 정수의 개수
         int s = Integer.parseInt(st.nextToken());   // 타겟
 
-        int[] arr1 = new int[n / 2];
-        int[] arr2 = new int[n - (n / 2)];
+        arr = new int[n];
         st = new StringTokenizer(bf.readLine());
-        for (int i = 0; i < n / 2; i++) {
-            arr1[i] = Integer.parseInt(st.nextToken());
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
         }
 
-        for (int i = 0; i < n - (n / 2); i++) {
-            arr2[i] = Integer.parseInt(st.nextToken());
-        }
+        Map<Integer, Integer> countMap = new HashMap<>();
 
-        // arr1 원소들의 조합으로 만들 수 있는 합
-        int[] sum1 = comb(arr1);
-
-        // arr2 원소들의 조합으로 만들 수 있는 합
-        int[] sum2 = comb(arr2);
-
-        // 이분탐색을 위한 정렬
-        Arrays.sort(sum2);
-
-        long answer = 0;
-        for (int sum : sum1) {
-            answer += upperBound(s - sum, sum2) - lowerBound(s - sum, sum2);
-        }
+        comb(0, n / 2, 0, sum -> countMap.put(sum, countMap.getOrDefault(sum, 0) + 1));
+        comb(n / 2, n, 0, sum -> answer += countMap.getOrDefault(s - sum, 0));
 
         System.out.println(s == 0 ? answer - 1 : answer);
     }
 
-    private static int upperBound(int target, int[] arr) {
-        int left = -1;
-        int right = arr.length;
-
-        while (left + 1 < right) {
-            int mid = ((left + right) >> 1);
-            if (arr[mid] > target) {
-                right = mid;
-            } else {
-                left = mid;
-            }
+    private static void comb(int startIdx, int endIdx, int sum, Consumer<Integer> callback) {
+        if (startIdx == endIdx) {
+            callback.accept(sum);
+            return;
         }
 
-        return right;
-    }
-
-    private static int lowerBound(int target, int[] arr) {
-        int left = -1;
-        int right = arr.length;
-
-        while (left + 1 < right) {
-            int mid = ((left + right) >> 1);
-            if (arr[mid] >= target) {
-                right = mid;
-            } else {
-                left = mid;
-            }
-        }
-
-        return right;
-    }
-
-    private static int[] comb(int[] arr) {
-        // 공집합 미포함
-        int[] sum = new int[1 << arr.length];
-
-        for (int i = 1; i < (1 << arr.length); i++) {
-            int s = 0;
-            for (int j = 0; j < arr.length; j++) {
-                if ((i & (1 << j)) > 0) {
-                    s += arr[j];
-                }
-            }
-
-            sum[i] = s;
-        }
-
-        return sum;
+        comb(startIdx + 1, endIdx, sum, callback);
+        comb(startIdx + 1, endIdx, sum + arr[startIdx], callback);
     }
 }
