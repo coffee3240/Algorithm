7fead38d7903cce5b6128c8327353391c9767301
coffee3240 <seungbok3240@gmail.com>
2025-05-07 22:53:20 +0900

    [level 2] Title: 조건에 맞는 개발자 찾기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/2/276034. 조건에 맞는 개발자 찾기/조건에 맞는 개발자 찾기.sql
+++ 프로그래머스/2/276034. 조건에 맞는 개발자 찾기/조건에 맞는 개발자 찾기.sql
@@ -1,6 +1,6 @@
 -- 코드를 작성해주세요
 select ID, EMAIL, FIRST_NAME, LAST_NAME
 from developers
-where skill_code & 256 = 256
-or skill_code & 1024 = 1024
+where (skill_code & (select code from skillcodes where name = 'C#')) > 0
+or (skill_code & (select code from skillcodes where name = 'Python')) > 0
 order by ID asc;
