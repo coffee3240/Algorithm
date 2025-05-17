71edbadb6ed02749d6ff65042b359916f21bff70
coffee3240 <seungbok3240@gmail.com>
2025-05-17 22:01:34 +0900

    [Gold III] Title: 게임 닉네임, Time: 1188 ms, Memory: 92332 KB -BaekjoonHub

---
+++ 백준/Gold/16934. 게임 닉네임/게임 닉네임.java
@@ -0,0 +1,68 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Main {
+
+    static int n;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+
+        n = Integer.parseInt(bf.readLine());
+
+        Trie trie = new Trie();
+        for (int i = 0; i < n; i++) {
+            System.out.println(trie.insert(bf.readLine()));
+        }
+    }
+
+    private static class Node {
+        int count;
+        Map<Character, Node> childMap = new HashMap<>();
+
+        public Map<Character, Node> getChildMap() {
+            return childMap;
+        }
+    }
+
+    private static class Trie {
+        Node root;
+
+        public Trie() {
+            this.root = new Node();
+        }
+
+        public String insert(String word) {
+            Node node = this.root;
+            StringBuilder alias = new StringBuilder();
+            boolean prefix = false;
+            for (int i = 0; i < word.length(); i++) {
+                char now = word.charAt(i);
+                if (!prefix) {
+                    alias.append(now);
+                }
+
+                if (!node.childMap.containsKey(now)) {
+                    prefix = true;
+                }
+
+                node = node.childMap.computeIfAbsent(now, w -> new Node());
+            }
+
+            node.count++;
+
+            if (prefix) {
+                return alias.toString();
+            } else {
+                if (node.count != 1) {
+                    alias.append(node.count);
+                }
+                
+                return alias.toString();
+            }
+        }
+    }
+}
