7b3300627327eb022bc8b229de25d63a6fefa28a
coffee3240 <seungbok3240@gmail.com>
2025-07-21 11:09:00 +0900

    [Gold III] Title: 체스판 위의 공, Time: 556 ms, Memory: 46040 KB -BaekjoonHub

---
+++ 백준/Gold/16957. 체스판 위의 공/체스판 위의 공.java
@@ -0,0 +1,100 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static int r, c;
+
+    static int[][] map;
+    static int[][] balls;
+
+    static Node[][] dp;
+
+    // <- 부터 시게방향
+    static int[] dy = {0, -1, -1, -1, 0, 1, 1, 1};
+    static int[] dx = {-1, -1, 0, 1, 1, 1, 0, -1};
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(bf.readLine());
+
+        r = Integer.parseInt(st.nextToken());
+        c = Integer.parseInt(st.nextToken());
+
+        map = new int[r][c];
+        for (int i = 0; i < r; i++) {
+            st = new StringTokenizer(bf.readLine());
+            for (int j = 0; j < c; j++) {
+                map[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        balls = new int[r][c];
+        dp = new Node[r][c];
+        for (int i = 0; i < r; i++) {
+            for (int j = 0; j < c; j++) {
+                Node node = solve(i, j);
+                balls[node.y][node.x]++;
+            }
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < r; i++) {
+            for (int j = 0; j < c; j++) {
+                sb.append(balls[i][j]).append(" ");
+            }
+            sb.append('\n');
+        }
+        System.out.println(sb);
+    }
+
+    private static Node solve(int y, int x) {
+        if (dp[y][x] != null) {
+            return dp[y][x];
+        }
+
+        int minY = y;
+        int minX = x;
+        int minValue = map[y][x];
+
+        for (int i = 0; i < 8; i++) {
+            int ny = y + dy[i];
+            int nx = x + dx[i];
+
+            if (OOB(ny, nx)) {
+                continue;
+            }
+
+            if (map[ny][nx] < minValue) {
+                minValue = map[ny][nx];
+                minY = ny;
+                minX = nx;
+            }
+        }
+
+        if (minY == y && minX == x) {   // 이동 X
+            dp[y][x] = new Node(y, x);
+        } else {    // 이동
+            Node next = solve(minY, minX);
+            dp[y][x] = new Node(next.y, next.x);
+        }
+
+        return dp[y][x];
+    }
+
+    private static boolean OOB(int y, int x) {
+        return y < 0 || y >= r || x < 0 || x >= c;
+    }
+
+    private static class Node {
+        int y;
+        int x;
+
+        public Node(int y, int x) {
+            this.y = y;
+            this.x = x;
+        }
+    }
+}
